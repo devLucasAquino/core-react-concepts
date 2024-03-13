cf666628b170bd1a0badbbf2ad1df440fdfd24a0
devLucas0110 <joaolucas.aqalves111@outlook.com>
2024-03-13 20:11:52 -0300

    fix: adjusted project folder organization

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,6 @@
-import reactImg from "./assets/react-core-concepts.png";
 import {CORE_CONCEPTS} from "./data.js";
-
-const reactDescription = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max){
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header(){
-  const description = reactDescription[genRandomInt(2)];
-
-  return(
-    <header>
-        <img src={reactImg} alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          {description} React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
-  );
-}
-
-function CoreConcept({image, title, description}){
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-}
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcept.jsx";
 
 function App() {
   return (
