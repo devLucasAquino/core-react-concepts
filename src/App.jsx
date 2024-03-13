0448b9912d40f5232628c5aab5d7f6ee41c0e388
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-03-13 17:28:39 -0300

    feat: used props properties

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
+import componentsImg from "./assets/components.png";
 
 const reactDescription = ['Fundamental', 'Crucial', 'Core'];
 
@@ -21,12 +22,36 @@ function Header(){
   );
 }
 
+function CoreConcept(props){
+  return (
+    <li>
+      <img src={props.image} alt={props.title} />
+      <h3>{props.title}</h3>
+      <p>{props.description}</p>
+    </li>
+  )
+}
+
 function App() {
   return (
     <div>
       <Header/>     
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept 
+            title="Components"
+            description="The core UI building block."
+            image={componentsImg}
+            />
+            <CoreConcept
+            title="Props"
+            />
+            <CoreConcept/>
+            <CoreConcept/>
+          </ul>
+        </section>
       </main>
     </div>
   );
