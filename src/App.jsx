c215567c1549f7bd0e55bdb7b3e2a38770a0ae5f
devLucas0110 <joaolucas.aqalves111@outlook.com>
2024-03-13 19:39:26 -0300

    added another choice of atribute in the core concept component

--- src/App.jsx
+++ src/App.jsx
@@ -45,21 +45,9 @@ function App() {
             description={CORE_CONCEPTS[0].description}
             image={CORE_CONCEPTS[0].image}
             />
-            <CoreConcept
-            title={CORE_CONCEPTS[1].title}
-            description={CORE_CONCEPTS[1].description}
-            image={CORE_CONCEPTS[1].image}
-            />
-            <CoreConcept
-            title={CORE_CONCEPTS[2].title}
-            description={CORE_CONCEPTS[2].description}
-            image={CORE_CONCEPTS[2].image}
-            />
-            <CoreConcept
-            title={CORE_CONCEPTS[3].title}
-            description={CORE_CONCEPTS[3].description}
-            image={CORE_CONCEPTS[3].image}
-            />
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
           </ul>
         </section>
       </main>
