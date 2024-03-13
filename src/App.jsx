e04fd4d9dc7c6530be28b6df0e43aff59e1da910
devLucas0110 <joaolucas.aqalves111@outlook.com>
2024-03-13 19:33:27 -0300

    finally core concepts part

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import reactImg from "./assets/react-core-concepts.png";
-import componentsImg from "./assets/components.png";
+import {CORE_CONCEPTS} from "./data.js";
 
 const reactDescription = ['Fundamental', 'Crucial', 'Core'];
 
@@ -41,15 +41,25 @@ function App() {
           <h2>Core Concepts</h2>
           <ul>
             <CoreConcept 
-            title="Components"
-            description="The core UI building block."
-            image={componentsImg}
+            title={CORE_CONCEPTS[0].title}
+            description={CORE_CONCEPTS[0].description}
+            image={CORE_CONCEPTS[0].image}
             />
             <CoreConcept
-            title="Props"
+            title={CORE_CONCEPTS[1].title}
+            description={CORE_CONCEPTS[1].description}
+            image={CORE_CONCEPTS[1].image}
             />
-            <CoreConcept/>
-            <CoreConcept/>
+            <CoreConcept
+            title={CORE_CONCEPTS[2].title}
+            description={CORE_CONCEPTS[2].description}
+            image={CORE_CONCEPTS[2].image}
+            />
+            <CoreConcept
+            title={CORE_CONCEPTS[3].title}
+            description={CORE_CONCEPTS[3].description}
+            image={CORE_CONCEPTS[3].image}
+            />
           </ul>
         </section>
       </main>
