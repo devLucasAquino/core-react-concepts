03d79f02d6f1ab33d8414a27ee168abec8177793
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-03-13 17:00:28 -0300

    feat: setting html atributtes dynamic load img

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,5 @@
+import reactImg from "./assets/react-core-concepts.png";
+
 const reactDescription = ['Fundamental', 'Crucial', 'Core'];
 
 function genRandomInt(max){
@@ -5,12 +7,14 @@ function genRandomInt(max){
 }
 
 function Header(){
+  const description = reactDescription[genRandomInt(2)];
+
   return(
     <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
+        <img src={reactImg} alt="Stylized atom" />
         <h1>React Essentials</h1>
         <p>
-          {reactDescription[genRandomInt(2)]} React concepts you will need for almost any app you are
+          {description} React concepts you will need for almost any app you are
           going to build!
         </p>
       </header>
