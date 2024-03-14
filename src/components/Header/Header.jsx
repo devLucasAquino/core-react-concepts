6d3c87c1777aa7da2bd656e12f16b76fc6ea6b38
devLucas0110 <joaolucas.aqalves111@outlook.com>
2024-03-13 21:06:36 -0300

    fix: general fix folder organizaded

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import reactImg from "../assets/react-core-concepts.png";
+import reactImg from "../../assets/react-core-concepts.png";
 import "./Header.css";
 
 const reactDescription = ['Fundamental', 'Crucial', 'Core'];
