2f04a9ea01f5a20af9cb248d5c676804589b5a7d
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-03-14 22:08:11 -0300

    feat: added dynamic styling

--- src/App.jsx
+++ src/App.jsx
@@ -47,10 +47,26 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton onSelect={() => handleSelect('components')}>Components</TabButton>
-            <TabButton onSelect={() => handleSelect('jsx')}>JSX</TabButton>
-            <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
-            <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
+            <TabButton 
+            isSelect={selectedTopic === 'components'}
+            onSelect={() => handleSelect('components')} >
+              Components
+            </TabButton>
+            <TabButton 
+            isSelect={selectedTopic === 'jsx'}
+            onSelect={() => handleSelect('jsx')}>
+              JSX
+            </TabButton>
+            <TabButton 
+            isSelect={selectedTopic === 'props'}
+            onSelect={() => handleSelect('props')}>
+            Props
+            </TabButton>
+            <TabButton 
+            isSelect={selectedTopic === 'state'}
+            onSelect={() => handleSelect('state')}> 
+              State
+            </TabButton>
           </menu>
           {tabContent}
         </section>
--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -1,8 +1,12 @@
-export default function TabButton({children, onSelect}){
+export default function TabButton({ children, onSelect, isSelect }){
 
     return(
         <li>
-            <button onClick={onSelect}>{children}</button>
+            <button 
+            className={ isSelect ? 'active' : undefined }
+            onClick={ onSelect} > 
+                { children }
+            </button>
         </li>
     )
 }
