34bb61cb99c19759b9aeacb7959eb42ba8d78bfc
devLucasAquino <joaolucas.aqalves111@outlook.com>
2024-03-14 21:45:37 -0300

    feat: added conditional content in App

--- src/App.jsx
+++ src/App.jsx
@@ -7,12 +7,25 @@ import TabButton from "./components/TabButton.jsx";
 import {EXAMPLES} from "./data.js";
 
 function App() {
-  const [ selectedTopic, setSelectedTopic ] = useState('components');
+  const [ selectedTopic, setSelectedTopic ] = useState();
 
   function handleSelect(selectedButton){
     setSelectedTopic(selectedButton)
   }
 
+  let tabContent = <p>Please select a topic.</p>;
+
+  if(selectedTopic){
+    tabContent = (
+      <div id="tab-content">
+            <h3>{EXAMPLES[selectedTopic].title}</h3>
+            <p>{EXAMPLES[selectedTopic].description}</p>
+            <pre>
+              <code>{EXAMPLES[selectedTopic].code}</code>
+            </pre>
+          </div>
+    );
+  }
 
   return (
     <div>
@@ -39,14 +52,7 @@ function App() {
             <TabButton onSelect={() => handleSelect('props')}>Props</TabButton>
             <TabButton onSelect={() => handleSelect('state')}>State</TabButton>
           </menu>
-          <div id="tab-content">
-            <h3>{EXAMPLES[selectedTopic].title}</h3>
-            <p>{EXAMPLES[selectedTopic].description}</p>
-            <pre>
-              <code>{EXAMPLES[selectedTopic].code}</code>
-            </pre>
-          </div>
-
+          {tabContent}
         </section>
       </main>
     </div>
